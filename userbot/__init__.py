8d79045e413db56b3010900e05d00e98ef48fdb3
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-04-04 11:20:48 +0530

    aria : fix download directory (#23)

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -183,7 +183,7 @@ G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET") or None
 G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA") or None
 G_DRIVE_FOLDER_ID = os.environ.get("G_DRIVE_FOLDER_ID") or None
 TEMP_DOWNLOAD_DIRECTORY = os.environ.get(
-    "TMP_DOWNLOAD_DIRECTORY") or "./downloads"
+    "TMP_DOWNLOAD_DIRECTORY") or "./downloads/"
 
 # Setting Up CloudMail.ru and MEGA.nz extractor binaries,
 # and giving them correct perms to work properly.
